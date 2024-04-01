fb8165a995b6631c6802a0a18f3effb1bfb115b8
LIUZHAO <liuzhaomax@163.com>
2024-04-01 22:15:33 +0800

    route

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import config from "./config"
 import short from "short-uuid"
 
 const initAxios = () => {
-    axios.defaults.baseURL = config.beBaseUrl
+    axios.defaults.baseURL = config.beBaseUrl + "/maxblog"
     axios.defaults.headers.common["Content-Type"] = "application/json"
     axios.defaults.headers.common["Request_id"] = short().new()
     axios.defaults.headers.common["App_id"] = short().new()
