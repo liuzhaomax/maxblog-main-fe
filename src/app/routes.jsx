e041c63bf3577793018f2b5b06cf98ef59af2ace
LIUZHAO <liuzhaomax@163.com>
2024-04-01 23:00:21 +0800

    route

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import {Suspense} from "react"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import Index from "../page/index/Index"
 import NotFound from "../page/notFound/NotFound"
 import Home from "../page/home/Home"
@@ -24,6 +24,11 @@ export default (
             <Route path={HOME.PATH} element={<Home/>}/>
         </Route>
         <Route element={lazyLoad(MAIN_LAYOUT.FILE_PATH)}>
+            {/*重定向*/}
+            <Route path="article" element={<Navigate to={ARTICLE.FULL_PATH}/>}/>
+            <Route path="demo" element={<Navigate to={DEMO.FULL_PATH}/>}/>
+            <Route path="project" element={<Navigate to={PROJECT.FULL_PATH}/>}/>
+            {/*路由*/}
             <Route path={ARTICLE.PATH} element={lazyLoad(ARTICLE.FILE_PATH)}/>
             <Route path={DEMO.PATH} element={lazyLoad(DEMO.FILE_PATH)}/>
             <Route path={PROJECT.PATH} element={lazyLoad(PROJECT.FILE_PATH)}/>
