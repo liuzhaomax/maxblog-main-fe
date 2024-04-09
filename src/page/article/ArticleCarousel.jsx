b9373a1c097d51b58fd4f378e6211afba58764e9
LIUZHAO <liuzhaomax@163.com>
2024-04-10 00:15:37 +0800

    carousel

--- src/page/article/ArticleCarousel.jsx
+++ src/page/article/ArticleCarousel.jsx
@@ -2,14 +2,6 @@ import React from "react"
 import "./ArticleCarousel.css"
 import { Carousel } from "antd"
 
-// const contentStyle = {
-//     height: "160px",
-//     color: "#fff",
-//     lineHeight: "160px",
-//     textAlign: "center",
-//     background: "#364d79",
-// }
-
 function ArticleCarousel() {
 
     return (
