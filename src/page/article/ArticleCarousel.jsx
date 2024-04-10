65f9af5872d384f4c0e89edc79832ce9d794b8c6
LIUZHAO <liuzhaomax@163.com>
2024-04-10 20:24:35 +0800

    走马灯

--- src/page/article/ArticleCarousel.jsx
+++ src/page/article/ArticleCarousel.jsx
@@ -1,24 +1,63 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./ArticleCarousel.css"
 import { Carousel } from "antd"
+import { getArticleList } from "./handlers"
+import config from "../../config/config"
+import { URL } from "../../config/url"
+import { useNavigate } from "react-router-dom"
 
 function ArticleCarousel() {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        loadCarouselList()
+    }, [])
+    const [articleListCarouselRes, setArticleListCarouselRes] = useState([])
+    const loadCarouselList = () => {
+        let params = {
+            pageNo: 1,
+            pageSize: 5,
+            tagName: "",
+            search: "",
+        }
+        getArticleList(params)
+            .then(res => {
+                let list = res.data.data.map(item => {
+                    return {
+                        id: item.id,
+                        cover: `${config.beBaseUrl}${URL.INNER.Static}${URL.INNER.Project}/${item.id}/${item.cover}`,
+                    }
+                })
+                setArticleListCarouselRes(list)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    const onClickCarouselItem = (id) => {
+        navigate(`${URL.INNER.ArticleArticle}/${id}`)
+    }
 
     return (
         <div className="article-carousel-container">
             <Carousel autoplay>
-                <div className="article-carousel-elem">
-                    1
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
+                {
+                    articleListCarouselRes ?
+                        articleListCarouselRes.map(item => {
+                            return (
+                                <img
+                                    className="article-carousel-elem"
+                                    alt="cover"
+                                    src={item.cover}
+                                    key={item.id}
+                                    onClick={() => onClickCarouselItem(item.id)}
+                                />
+                            )
+                        })
+                        :
+                        <></>
+                }
             </Carousel>
         </div>
     )
