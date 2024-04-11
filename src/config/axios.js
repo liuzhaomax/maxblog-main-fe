74f9a3cd0a1b72cadfa9d12aad77db72c580dd15
LIUZHAO <liuzhaomax@163.com>
2024-04-11 19:54:16 +0800

    header

--- src/config/axios.js
+++ src/config/axios.js
@@ -6,7 +6,6 @@ import { URL } from "./url"
 const initAxios = () => {
     axios.defaults.baseURL = config.beBaseUrl + URL.INNER.Maxblog
     axios.defaults.headers.common["Content-Type"] = "application/json"
-    axios.defaults.headers.common["Request_id"] = short().new()
     axios.defaults.headers.common["App_id"] = short().new()
 }
 
--- src/page/article/handlers.js
+++ src/page/article/handlers.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 import { URL } from "../../config/url"
+import short from "short-uuid"
 
 export const getArticleList = (params) => {
+    axios.defaults.headers.common["Request_id"] = short().new()
     if (params) {
         return axios.get(URL.INNER.ArticleList +
             `?pageNo=${params.pageNo}&pageSize=${params.pageSize}&tagName=${params.tagName}&search=${params.search}`)
@@ -10,13 +12,16 @@ export const getArticleList = (params) => {
 }
 
 export const getArticleTags = () => {
+    axios.defaults.headers.common["Request_id"] = short().new()
     return axios.get(URL.INNER.ArticleTags)
 }
 
 export const getArticleArticle = (id) => {
+    axios.defaults.headers.common["Request_id"] = short().new()
     return axios.get(URL.INNER.ArticleArticle + `?articleId=${id}`)
 }
 
 export const patchArticleArticle = (params) => {
+    axios.defaults.headers.common["Request_id"] = short().new()
     return axios.patch(URL.INNER.ArticleArticle + `?articleId=${params.id}&field=${params.field}`)
 }
