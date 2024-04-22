b53bf73a401f2efd41bfdfa0652db4339daf4b7f
LIUZHAO <liuzhaomax@163.com>
2024-04-22 10:22:12 +0800

    admin

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -35,7 +35,7 @@ function Footer() {
                         <h3>相关资源</h3>
                         <a href="https://github.com/liuzhaomax/go-maxms" target="_blank" rel="noreferrer">后端脚手架</a>
                         <a href="https://github.com/liuzhaomax/go-maxms-react" target="_blank" rel="noreferrer">前端脚手架</a>
-                        <a href="https://space.bilibili.com/18359348?spm_id_from=333.1007.0.0" target="_blank" rel="noreferrer">开发教程</a>
+                        <a href="https://space.bilibili.com/18359348?spm_id_from=333.1007.0.0" target="_blank" rel="noreferrer">脚手架使用教程</a>
                     </div>
                     <div className="footer-upper-nav">
                         <h3>贡献者</h3>
@@ -44,10 +44,14 @@ function Footer() {
                     <div className="footer-upper-nav">
                         <h3>管理入口</h3>
                         <a href="http://106.15.94.179:9602" target="_blank" rel="noreferrer">CMS平台</a>
-                        <a href="" target="_blank" rel="noreferrer">Monitor</a>
-                        <a href="https://mahara.infotech.monash.edu/view/view.php?id=56000" target="_blank" rel="noreferrer">Mahara SM Page</a>
-                        <a href="https://mahara.infotech.monash.edu/view/view.php?id=56002" target="_blank" rel="noreferrer">Mahara IM Page</a>
-                        <a href="https://monashie.leankit.com/board/1602422272" target="_blank" rel="noreferrer">LeanKit Board</a>
+                        <a href="https://jenkins.liuzhaomax.cn" target="_blank" rel="noreferrer">Jenkins</a>
+                        <a href="https://vault.liuzhaomax.cn" target="_blank" rel="noreferrer">Vault</a>
+                        <a href="https://consul.liuzhaomax.cn" target="_blank" rel="noreferrer">Consul</a>
+                        <a href="https://prometheus.liuzhaomax.cn" target="_blank" rel="noreferrer">Prometheus</a>
+                        <a href="https://grafana.liuzhaomax.cn" target="_blank" rel="noreferrer">Grafana</a>
+                        <a href="https://kibana.liuzhaomax.cn" target="_blank" rel="noreferrer">Kibana</a>
+                        <a href="https://jaeger.liuzhaomax.cn" target="_blank" rel="noreferrer">Jaeger</a>
+                        <a href="http://106.15.94.179:9877" target="_blank" rel="noreferrer">RocketMQ</a>
                     </div>
                 </div>
                 <hr/>
