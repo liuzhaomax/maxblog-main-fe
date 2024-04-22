dbf304dd2a13d2f61ebdbf107e1064917d7411fa
LIUZHAO <liuzhaomax@163.com>
2024-04-22 10:43:46 +0800

    footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -33,25 +33,31 @@ function Footer() {
                     <img id="footer-logo" className="footer-upper-wrap" src={logo} alt={"logo"}/>
                     <div className="footer-upper-nav">
                         <h3>相关资源</h3>
-                        <a href="https://github.com/liuzhaomax/go-maxms" target="_blank" rel="noreferrer">后端脚手架</a>
-                        <a href="https://github.com/liuzhaomax/go-maxms-react" target="_blank" rel="noreferrer">前端脚手架</a>
-                        <a href="https://space.bilibili.com/18359348?spm_id_from=333.1007.0.0" target="_blank" rel="noreferrer">脚手架使用教程</a>
+                        <div className="footer-upper-nav-item-wrap">
+                            <a href="https://github.com/liuzhaomax/go-maxms" target="_blank" rel="noreferrer">后端脚手架</a>
+                            <a href="https://github.com/liuzhaomax/go-maxms-react" target="_blank" rel="noreferrer">前端脚手架</a>
+                            <a href="https://space.bilibili.com/18359348?spm_id_from=333.1007.0.0" target="_blank" rel="noreferrer">脚手架使用教程</a>
+                        </div>
                     </div>
                     <div className="footer-upper-nav">
                         <h3>贡献者</h3>
-                        <div id="CONTRIBUTORS"></div>
+                        <div className="footer-upper-nav-item-wrap">
+                            <div id="CONTRIBUTORS"></div>
+                        </div>
                     </div>
                     <div className="footer-upper-nav">
                         <h3>管理入口</h3>
-                        <a href="http://106.15.94.179:9602" target="_blank" rel="noreferrer">CMS平台</a>
-                        <a href="https://jenkins.liuzhaomax.cn" target="_blank" rel="noreferrer">Jenkins</a>
-                        <a href="https://vault.liuzhaomax.cn" target="_blank" rel="noreferrer">Vault</a>
-                        <a href="https://consul.liuzhaomax.cn" target="_blank" rel="noreferrer">Consul</a>
-                        <a href="https://prometheus.liuzhaomax.cn" target="_blank" rel="noreferrer">Prometheus</a>
-                        <a href="https://grafana.liuzhaomax.cn" target="_blank" rel="noreferrer">Grafana</a>
-                        <a href="https://kibana.liuzhaomax.cn" target="_blank" rel="noreferrer">Kibana</a>
-                        <a href="https://jaeger.liuzhaomax.cn" target="_blank" rel="noreferrer">Jaeger</a>
-                        <a href="http://106.15.94.179:9877" target="_blank" rel="noreferrer">RocketMQ</a>
+                        <div className="footer-upper-nav-item-wrap">
+                            <a href="http://106.15.94.179:9602" target="_blank" rel="noreferrer">CMS平台</a>
+                            <a href="https://jenkins.liuzhaomax.cn" target="_blank" rel="noreferrer">Jenkins</a>
+                            <a href="https://vault.liuzhaomax.cn" target="_blank" rel="noreferrer">Vault</a>
+                            <a href="https://consul.liuzhaomax.cn" target="_blank" rel="noreferrer">Consul</a>
+                            <a href="https://prometheus.liuzhaomax.cn" target="_blank" rel="noreferrer">Prometheus</a>
+                            <a href="https://grafana.liuzhaomax.cn" target="_blank" rel="noreferrer">Grafana</a>
+                            <a href="https://kibana.liuzhaomax.cn" target="_blank" rel="noreferrer">Kibana</a>
+                            <a href="https://jaeger.liuzhaomax.cn" target="_blank" rel="noreferrer">Jaeger</a>
+                            <a href="http://106.15.94.179:9877" target="_blank" rel="noreferrer">RocketMQ</a>
+                        </div>
                     </div>
                 </div>
                 <hr/>
