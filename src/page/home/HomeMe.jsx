210fc42f7a2bb1ddaa055c8a3cb3e37acf1d59fa
LIUZHAO <liuzhaomax@163.com>
2024-05-04 19:28:54 +0800

    homeme

--- src/page/home/HomeMe.jsx
+++ src/page/home/HomeMe.jsx
@@ -1,9 +1,103 @@
 import "./HomeMe.css"
 import React from "react"
+import imgPortrait from "../../asset/announcement/portrait.png"
 
 const HomeMe = () => {
     return (
         <div id="home-me" className="home-me">
+            <div className="home-me-container">
+                <div className="home-me-west home-me-wrap">
+                    <a href="https://github.com/liuzhaomax" target="_blank" rel="noreferrer">
+                        <img src={imgPortrait} alt="home_me_portrait" className="home-me-portrait" />
+                    </a>
+                    <div className="home-me-badge-wrap">
+                        <a href="https://github.com/liuzhaomax" target="_blank" rel="noreferrer">
+                            <img src="https://img.shields.io/badge/GitHub-s?style=flat-square&logo=github&logoColor=white&color=%23333" alt="skill-tag" className="home-me-skill-tag"/>
+                        </a>
+                        <a href="https://space.bilibili.com/18359348?spm_id_from=333.1007.0.0" target="_blank" rel="noreferrer">
+                            <img src="https://img.shields.io/badge/Bilibili-fb7299?style=flat-square&logo=Bilibili&logoColor=white" alt="skill-tag" className="home-me-skill-tag"/>
+                        </a>
+                        <img src="https://img.shields.io/badge/Golang-s?style=flat-square&logo=go&logoColor=white&color=%2300a7d0" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/JavaScript-s?style=flat-square&logo=javascript&logoColor=white&color=%23e05d44" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Python-c99e03?style=flat-square&logo=python&logoColor=white" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Gin-0090d1?style=flat-square&logo=gin&logoColor=white" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/gRPC-s?style=flat-square&logo=go&logoColor=white&color=%2300a7d0" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Gorm-s?style=flat-square&logo=go&logoColor=white&color=%2300a7d0" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/React-00b2e3?style=flat-square&logo=react&logoColor=white" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Vue-3fb27f?style=flat-square&logo=Vue.js&logoColor=white" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Express-444444?style=flat-square&logo=express&logoColor=white" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Spark-s?style=flat-square&logo=apachespark&logoColor=white&color=%23F55B14" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Jenkins-s?style=flat-square&logo=jenkins&logoColor=white&color=%23333" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Docker-2496ed?style=flat-square&logo=docker&logoColor=white" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Kubernetes-s?style=flat-square&logo=Kubernetes&logoColor=white&color=%233069de" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Vault-s?style=flat-square&logo=vault&logoColor=white&color=%23ffd814" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Consul-s?style=flat-square&logo=consul&logoColor=white&color=%23c0296d" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/ETCD-s?style=flat-square&logo=etcd&logoColor=white&color=%23419eda" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Prometheus-s?style=flat-square&logo=Prometheus&logoColor=white&color=%23d44a2d" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Grafana-s?style=flat-square&logo=Grafana&logoColor=white&color=%23f3602e" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Elasticserarch-s?style=flat-square&logo=elastic&logoColor=white&color=%2300bfb3" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Filebeat-s?style=flat-square&logo=elastic&logoColor=white&color=%2300bfb3" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Kibana-s?style=flat-square&logo=kibana&logoColor=white&color=%2300bfb3" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Jaeger-s?style=flat-square&logo=jaeger&logoColor=white&color=%231f94aa" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/RocketMQ-s?style=flat-square&logo=apacherocketmq&logoColor=white&color=%23d06f0f" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/SonarQube-s?style=flat-square&logo=sonarqube&logoColor=white&color=%233b8fc4" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Harbor-s?style=flat-square&logo=harbor&logoColor=white&color=%2300364d" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Postman-s?style=flat-square&logo=postman&logoColor=white&color=%23ff6c37" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/OCP4-s?style=flat-square&logo=redhat&logoColor=white&color=%23ee0000" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Nginx-s?style=flat-square&logo=nginx&logoColor=white&color=%23009137" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/MySQL-s?style=flat-square&logo=mysql&logoColor=white&color=%23005e86" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Redis-s?style=flat-square&logo=redis&logoColor=white&color=%23d5362b" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/MongoDB-s?style=flat-square&logo=mongodb&logoColor=white&color=%2300684a" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Oracle-s?style=flat-square&logo=oracle&logoColor=white&color=%23e81621" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Splunk-s?style=flat-square&logo=splunk&logoColor=white&color=%23ce0070" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/NewRelic-s?style=flat-square&logo=NewRelic&logoColor=white&color=%231ce783" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Mongoose-s?style=flat-square&logo=Mongoose&logoColor=white&color=%23800" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/AntD-s?style=flat-square&logo=antdesign&logoColor=white&color=%231677ff" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/OpenAPI-s?style=flat-square&logo=Swagger&logoColor=white&color=%2394c73d" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Jira-s?style=flat-square&logo=jira&logoColor=white&color=%231868db" alt="skill-tag" className="home-me-skill-tag"/>
+                        <img src="https://img.shields.io/badge/Confluence-s?style=flat-square&logo=confluence&logoColor=white&color=%231868db" alt="skill-tag" className="home-me-skill-tag"/>
+                    </div>
+                </div>
+                <div className="home-me-east home-me-wrap">
+                    <h2>技术栈</h2>
+                    <p>
+                        <span className="home-me-skill-subtitle">综合：</span>
+                        设计模式，分布式架构，微服务架构，云平台应用，DevOps理论，CI/CD实践，
+                        代码管理与版本控制 (GitHub, GitLab, Gogs), 敏捷开发实践经验(Jira, Confluence等),
+                        HTTP/HTTPS (RESTful api, GraphQL), websocket, TCP, RPC (gRPC),
+                        加密 (对称加密, RSA), 测试, JWT, opentracing, 中间件开发，数据可视化，swagger openAPI,
+                        项目、产品、团队管理实践经验, 高等教育与金融行业经验
+                    </p>
+                    <p>
+                        <span className="home-me-skill-subtitle">后端：</span>
+                        golang (gin, wire, gorm, grpc, ginkgo等), node.js (express, mongoose), consul, etcd, vault,
+                        rocketMQ, jaeger, prometheus, grafana, elasticsearch, filebeat, kibana, sentinel, splunk,
+                        new relic等
+                    </p>
+                    <p>
+                        <span className="home-me-skill-subtitle">前端：</span>
+                        react (react-router, redux, antd, antv等), vue2 (vue-router, vuex, iview等), axios, 等
+                    </p>
+                    <p>
+                        <span className="home-me-skill-subtitle">数据库与缓存：</span>
+                        mysql, redis, mongodb, oracle
+                    </p>
+                    <p>
+                        <span className="home-me-skill-subtitle">运维：</span>
+                        阿里云, GCP, linux (centOs7), K8s (OCP4), docker, nginx, jenkins, sonarqube, harbor,
+                        checkmarx, blackduck, 日志与监控(见后端)，GitHub (workflow, webhook)
+                    </p>
+                    <p>
+                        <span className="home-me-skill-subtitle">测试：</span>
+                        mountebank, ginkgo, jmeter, postman
+                    </p>
+                    <p>
+                        <span className="home-me-skill-subtitle">其他：</span>
+                        大数据 (pySpark, 机器学习pipeline, 数据仓库, 等), GUI (pyQT5), Matlab (Simulink),
+                        java, android, 微信小程序, AI基础理论, 等
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
